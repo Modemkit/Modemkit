114e61c619c9c9048c52d502979acc5aa3479a08
BalconyJH <balconyjh@gmail.com>
2026-03-18 14:33:01 +0800

    :tada: Add project files and configuration for Ymodem.Protocol and its tests

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "StyleCop.CSharp.DocumentationRules",
+    "SA0001:XML comment analysis is disabled due to project configuration",
+    Justification = "Repository-level StyleCop configuration disables XML documentation requirements for now.")]
